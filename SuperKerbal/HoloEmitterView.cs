fb3dab503f8aba4487f1a63a1c3f78c3752c66ef
Michael Billard <angel125@spellflight.com>
2017-02-26 09:01:38 -0800

    Mobile Emitter Rebranding

---
+++ SuperKerbal/HoloEmitterView.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP.IO;
+using Experience;
+using KIS;
+
+/*
+Source code copyrighgt 2016, by Martystu Kerman
+License: GNU General Public License Version 3
+License URL: http://www.gnu.org/licenses/
+*/
+namespace SuperKerbal
+{
+    public delegate void SetTraitDelegate(string traitName);
+    public delegate void ToggleHighlightDelegate(bool isHighlighted);
+    public delegate void ToggleHelmetDelegate(bool showHelmet);
+
+    public class HoloEmitterView : Window<HoloEmitterView>
+    {
+        public SetTraitDelegate setTrait;
+        public ToggleHighlightDelegate toggleHighlight;
+        public ToggleHelmetDelegate setHelmetVisible;
+
+        public string currentTraitName = string.Empty;
+        public bool isHighlighted = false;
+        public bool showHelmet = true;
+        public ExperienceSystemConfig expSysConfig;
+        public ProtoCrewMember crewMember;
+
+        private Vector2 scrollPos = new Vector2();
+        private string[] traitNames;
+
+        public HoloEmitterView() :
+        base("Mobile Emitter", 300, 400)
+        {
+            Resizable = false;
+        }
+
+        public override void SetVisible(bool newValue)
+        {
+            base.SetVisible(newValue);
+
+            //Get trait names
+            expSysConfig = new ExperienceSystemConfig();
+            expSysConfig.LoadTraitConfigs();
+            traitNames = expSysConfig.TraitNames.ToArray();
+        }
+
+        protected override void DrawWindowContents(int windowId)
+        {
+            GUILayout.BeginVertical();
+
+            GUILayout.Label("<color=white>Please state the nature of your in-flight emergency:</color>");
+
+            //Toggle highlight
+            if (toggleHighlight != null)
+            {
+                if (GUILayout.Button("Hologram highlighting on/off"))
+                {
+                    isHighlighted = !isHighlighted;
+                    toggleHighlight(isHighlighted);
+                }
+            }
+
+            //Toggle badS
+            crewMember.isBadass = GUILayout.Toggle(crewMember.isBadass, "Activate Fearless subroutine");
+
+            //Toggle helmet
+            if (setHelmetVisible != null)
+            {
+                showHelmet = GUILayout.Toggle(showHelmet, "Render helmet");
+                setHelmetVisible(showHelmet);
+            }
+
+            //Set trait
+            GUILayout.Label("<color=white>Current Program: " + currentTraitName + "</color>");
+            GUILayout.Label("<color=white>Activate emergency program:</color>");
+            scrollPos = GUILayout.BeginScrollView(scrollPos);
+            for (int index = 0; index < traitNames.Length; index++)
+            {
+                if (GUILayout.Button(traitNames[index]) && setTrait != null)
+                {
+                    currentTraitName = traitNames[index];
+                    setTrait(traitNames[index]);
+                }
+            }
+            GUILayout.EndScrollView();
+
+            GUILayout.EndVertical();
+        }
+    }
+}
---
+++ SuperKerbal/ModuleHoloEmitter.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP.IO;
+using Experience;
+using KIS;
+
+/*
+Source code copyrighgt 2016, by Martystu Kerman
+License: GNU General Public License Version 3
+License URL: http://www.gnu.org/licenses/
+*/
+namespace SuperKerbal
+{
+    /*
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, true)]
+    public class SuperKerbalAddon : MonoBehaviour
+    {
+        public void Awake()
+        {
+            Part evaPrefab = PartLoader.getPartInfoByName("kerbalEVA").partPrefab;
+            try { evaPrefab.AddModule("ModuleHoloEmitter"); }
+            catch { }
+
+            evaPrefab = PartLoader.getPartInfoByName("kerbalEVAfemale").partPrefab;
+            try { evaPrefab.AddModule("ModuleHoloEmitter"); }
+            catch { }
+        }
+    }
+     */
+
+    public class ModuleHoloEmitter : ModuleKISItem
+    {
+        [KSPField(isPersistant = true)]
+        public string currentTraitName = string.Empty;
+
+        [KSPField(isPersistant = true)]
+        public bool isHighlighted = false;
+
+        [KSPField(isPersistant = true)]
+        public bool showHelmet = true;
+
+        protected ExperienceSystemConfig expSysConfig;
+        protected KIS.ModuleKISInventory inventory;
+        protected HoloEmitterView holoEmitterView = new HoloEmitterView();
+        protected Color highlightColor = new Color(0, 191, 243);
+
+        [KSPEvent(guiName = "Activate Emitter", guiActive = true)]
+        public void ActivateEmitter()
+        {
+            Vessel vessel = FlightGlobals.ActiveVessel;
+            ProtoCrewMember crewMember = vessel.GetVesselCrew()[0];
+
+            //Show the holo emitter screen.
+            holoEmitterView.crewMember = crewMember;
+            holoEmitterView.currentTraitName = currentTraitName;
+            holoEmitterView.isHighlighted = isHighlighted;
+            holoEmitterView.showHelmet = showHelmet;
+            holoEmitterView.SetVisible(!holoEmitterView.IsVisible());
+        }
+
+        public void OnGUI()
+        {
+            if (holoEmitterView.IsVisible())
+                holoEmitterView.DrawWindow();
+        }
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            if (HighLogic.LoadedSceneIsFlight == false)
+                return;
+
+            //Setup the inventory object.
+            inventory = this.part.vessel.rootPart.FindModuleImplementing<KIS.ModuleKISInventory>();
+            
+            //Set up the system config
+            expSysConfig = new ExperienceSystemConfig();
+            expSysConfig.LoadTraitConfigs();
+
+            //Setup view
+            holoEmitterView.toggleHighlight = ToggleHighlight;
+            holoEmitterView.setTrait = SetTrait;
+            holoEmitterView.setHelmetVisible = SetHelmetVisible;
+
+            //Set current trait if needed
+            if (string.IsNullOrEmpty(currentTraitName))
+            {
+                Vessel vessel = FlightGlobals.ActiveVessel;
+                ProtoCrewMember crewMember = vessel.GetVesselCrew()[0];
+
+                currentTraitName = crewMember.trait;
+            }
+
+            //Set highlighting and such
+            ToggleHighlight(isHighlighted);
+            SetTrait(currentTraitName);
+            SetHelmetVisible(showHelmet);
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            if (HighLogic.LoadedSceneIsFlight == false)
+                return;
+
+            if (isHighlighted)
+                this.part.vessel.rootPart.Highlight(highlightColor);
+        }
+
+        public void SetHelmetVisible(bool isVisible)
+        {
+            if (isVisible == showHelmet)
+                return;
+
+            Vessel vessel = FlightGlobals.ActiveVessel;
+            ProtoCrewMember crewMember = vessel.GetVesselCrew()[0];
+
+            showHelmet = isVisible;
+
+            //Setup inventory object if needed.
+            if (inventory == null)
+                inventory = this.part.vessel.rootPart.FindModuleImplementing<KIS.ModuleKISInventory>();
+
+            if (inventory != null)
+                inventory.SetHelmet(isVisible);
+            else
+                Debug.Log("[ModuleHoloEmitter] - inventory object is null");
+        }
+
+        public void ToggleHighlight(bool isHighlighted)
+        {
+            this.isHighlighted = isHighlighted;
+
+            //Turn off the highlighting if it's been toggled off.
+            if (isHighlighted == false)
+            {
+                this.part.Highlight(false);
+                return;
+            }
+
+            //Highlight the part.
+            this.part.vessel.rootPart.Highlight(highlightColor);
+        }
+
+        public void SetTrait(string traitName)
+        {
+            Vessel vessel = FlightGlobals.ActiveVessel;
+            ProtoCrewMember crewMember = vessel.GetVesselCrew()[0];
+
+            if (expSysConfig.TraitNames.Contains(traitName) == false)
+                return;
+
+            //Set the kerbal experience trait
+            currentTraitName = traitName;
+
+            //Setup inventory object if needed.
+            if (inventory == null)
+                inventory = this.part.vessel.rootPart.FindModuleImplementing<KIS.ModuleKISInventory>();
+
+            //Set new trait
+            KerbalRoster.SetExperienceTrait(crewMember, traitName);
+            if (inventory != null)
+                inventory.kerbalTrait = traitName;
+            else
+                Debug.Log("[ModuleHoloEmitter] - inventory object is null");
+        }
+    }
+}
